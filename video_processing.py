620c4a3580f873e3f74bc3d29a83a26e6ccfc41b
danieltao <chaofan.tao@gmail.com>
2020-11-18 16:40:13 -0500

    add video frame capture:

---
+++ video_processing.py
@@ -0,0 +1,40 @@
+# Program To Read video
+# and Extract Frames
+import cv2
+import os
+
+# Function to extract frames
+def frame_capture(path, video):
+    # Path to video file
+    print("Start capturing frames for ", video)
+    vidObj = cv2.VideoCapture(path)
+
+    # Used as counter variable
+    count = 0
+    if not os.path.exists("original-frames/" + video+"_frames"):
+        os.mkdir("original-frames/" + video+"_frames")
+    else:
+        print("did frame capture before")
+        return len(os.listdir("original-frames/" + video+"_frames"))
+    # checks whether frames were extracted
+    success = 1
+
+    while success:
+        # vidObj object calls read
+        # function extract frames
+        success, image = vidObj.read()
+
+        # Saves the frames with frame-count
+        cv2.imwrite("original-frames/" + video + "_frames/" + video+"%05d.jpeg" % count, image)
+
+        count += 1
+        if count % 100 == 0:
+            print("finished capturing frame "+ str(count) + " video " + video)
+
+    return count
+
+if __name__ == '__main__':
+    # Calling the function
+    path = "/usr/xtmp/ct214/daml/vr_sickness/flownet2-pytorch/skyhouse_perspective.mp4"
+    name = "skyhouse_perspective"
+    frame_capture(path, video=name)
